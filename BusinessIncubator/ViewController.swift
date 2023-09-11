54cd16379d94355b71a97254f31c12ff2da2aee0
Jake Kenefick <jake.kenefick24@d155.org>
2023-09-11 09:13:50 -0500

    Initial Commit

---
+++ BusinessIncubator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusinessIncubator
+//
+//  Created by JAKE KENEFICK on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
