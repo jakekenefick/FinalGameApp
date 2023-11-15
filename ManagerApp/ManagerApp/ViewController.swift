f84b1e9a9af1459d90981dfc55fea239c5a69eba
Liam <jacox0952@gmail.com>
2023-11-15 09:16:06 -0600

    added classes

--- ManagerApp/ManagerApp/ViewController.swift
+++ ManagerApp/ManagerApp/ViewController.swift
@@ -5,6 +5,27 @@
 //  Created by JAKE KENEFICK on 11/14/23.
 //
 
+public class AppData {
+    static var employees = [String]()
+}
+class Employee {
+    var name: String
+    var role: String
+    var age: Int
+    var wage: Double
+    var hours: Double
+    var bonus: Double
+    
+    public init(name: String, role: String, age: Int, wage: Double, hours: Double, bonus: Double) {
+        self.name = name
+        self.role = role
+        self.age = age
+        self.wage = wage
+        self.hours = hours
+        self.bonus = bonus
+    }
+}
+
 import UIKit
 
 class ViewController: UIViewController {
